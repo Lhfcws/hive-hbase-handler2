d802eef7a4359a076a485ea735e5e75ec96fe453
lhfcws <wenjie@datatub.com>
2017-08-22 18:54:41 +0800

    debug

--- src/main/java/org/apache/hadoop/hive/hbase/HiveHBaseTableInputFormat2.java
+++ src/main/java/org/apache/hadoop/hive/hbase/HiveHBaseTableInputFormat2.java
@@ -5,9 +5,15 @@ import org.apache.hadoop.mapred.InputSplit;
 import org.apache.hadoop.mapred.JobConf;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
+ * org.apache.hadoop.hive.hbase.HiveHBaseTableInputFormat2
+ *
  * HiveHBaseTableInputFormat implements InputFormat for HBase storage handler
  * tables, decorating an underlying HBase TableInputFormat with extra Hive logic
  * such as column pruning and filter pushdown.
@@ -17,7 +23,23 @@ public class HiveHBaseTableInputFormat2 extends HiveHBaseTableInputFormat {
     @Override
     public InputSplit[] getSplits(JobConf jobConf, int numSplits) throws IOException {
         LOG.info("[HBASE] " + jobConf.get("hbase.zookeeper.quorum"));
+        LOG.info("[HiveHBaseTableInputFormat2 classpath] " + getClasspaths());
         return super.getSplits(jobConf, numSplits);
+    }
+
+    private static List<String> getClasspaths() {
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+        LinkedList ret = new LinkedList();
+        URL[] arr$ = urls;
+        int len$ = urls.length;
+
+        for(int i$ = 0; i$ < len$; ++i$) {
+            URL url = arr$[i$];
+            ret.add(url.getFile());
+        }
+
+        return ret;
     }
 
     public static void main(String[] args) throws Exception {
